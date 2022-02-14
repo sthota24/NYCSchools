9b2f2f780c750a62279773b0c160f8c0c762a134
Srikanth Thota <srikanth.thota@vida.com>
2022-02-13 22:10:55 -0800

    Initial Commit

---
+++ 20220213-SrikanthThota-NYCSchoolsTests/_0220213_SrikanthThota_NYCSchoolsTests.swift
@@ -0,0 +1,33 @@
+//
+//  _0220213_SrikanthThota_NYCSchoolsTests.swift
+//  20220213-SrikanthThota-NYCSchoolsTests
+//
+//  Created by Srikanth on 2/13/22.
+//
+
+import XCTest
+@testable import _0220213_SrikanthThota_NYCSchools
+
+class _0220213_SrikanthThota_NYCSchoolsTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
